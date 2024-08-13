736cf03354e5c99e38c3bc3462424a6fc5924c50
ChaiwitMaitree <103880314+ChaiwitMaitree@users.noreply.github.com>
2024-08-13 23:47:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+from myserver import server_on 
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+
+
+
+
+# คำสั่งBot พร้อมใช้งาน
+
+
+@bot.event
+async def on_ready():
+    print("BOT Online!")
+
+#แจ้งคนเข้าออก
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1272601097412018187)
+    text = f"{member.name} ออกจากเชิฟเวอร์แล้ว!"
+
+    emmbed = discord.embed (title = 'ออกจากเชิฟเวอร์แล้ว',description = 'โชคดีแล้วพบกันใหม่นั้มม')
+    await channel.send(embed = emmbed)
+    await channel.send(text)
+    
+server_on()
+
+bot.run(os.getenv('TOKEN'))
